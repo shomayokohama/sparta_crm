e680398ac630373ce5e34852bbcd282456cc4154
横浜翔馬 <yokohamashoma@yokohama.local>
2015-09-12 14:04:47 +0900

    gemのカミナリを入れた

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,15 @@
 class Customer < ActiveRecord::Base
-  def full_name
-    full_name = family_name + given_name
-    full_name
+
+  validates :family_name,
+    presence: true, length: {maximum: 20 }
+  validates :given_name,
+    presence: true, length: {maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+    presence: true, format: { with: VALID_EMAIL_REGEX}
+
+  def full_name(keisho)
+    full_name = family_name + given_name + keisho
+    return full_name
   end
 end
