829314c4a33941be7b07b9cdecdf6b5b6182ec76
横浜翔馬 <yokohamashoma@yokohama.local>
2015-09-13 08:07:24 +0900

    仮コミット

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_customer ,only: [:show, :edit, :destroy, :update]
 
   def index
